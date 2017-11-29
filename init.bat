22301e3659f1042f46eaaf64f4b727e114f2b3fb
jesse23 <pengwenjia@gmail.com>
2017-11-29 10:02:43 -0500

    Initial update

---
+++ init.bat
@@ -0,0 +1,5 @@
+CALL npm install cordova ionic
+set PATH=.\node_modules\.bin;%PATH%
+set NODE_HOME=D:\ugs\node-v6.11.3-win-x64
+set PATH=%NODE_HOME%;%NODE_HOME%\node_modules\npm\bin\node-gyp-bin;%PATH%
+
