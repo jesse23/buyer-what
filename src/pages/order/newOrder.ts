6813c3cd1bfca576b0aee0804db50d4adf5517a2
jesse23 <pengwenjia@gmail.com>
2017-12-11 17:52:28 -0500

    daily submit

--- src/pages/order/newOrder.ts
+++ src/pages/order/newOrder.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-newOrder',
   templateUrl: 'newOrder.html'
 })
 export class NewOrderPage {
+
+  constructor(public navCtrl: NavController) {}
   
-  constructor() {}
+  createOrder() {
+    this.navCtrl.pop();
+  }
 }
